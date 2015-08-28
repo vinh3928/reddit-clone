472c9de236dd3a529eff7da97da307e4d90b8c5a
Vinh Nguyen <vnguyen.cheme@gmail.com>
2015-08-28 14:35:38 -0600

    added firebase database

--- js/controller.js
+++ js/controller.js
@@ -1,102 +1,114 @@
-var myApp = angular.module("reddit", ["ngAnimate"]);
+var myApp = angular.module("reddit", ["ngAnimate", "firebase"]);
 
-myApp.controller("users", ["$scope", function($scope) {
-  $scope.posts = [
-    {title: "Aspen, CO",
-      vote: -9,
-      author: "Ned Stark",
-      content: "Vestibulum id urna nec massa congue gravida a ut nisi. Nullam sit amet urna rutrum, faucibus quam quis, sagittis ipsum. Integer condimentum, felis id ultricies blandit, velit eros imperdiet elit, ut vulputate arcu purus sed elit. Morbi efficitur cursus dictum. Cras sagittis sapien vel quam euismod feugiat. Cras a risus quis libero venenatis aliquam. In tristique maximus convallis.Vestibulum id urna nec massa congue gravida a ut nisi. Nullam sit amet urna rutrum, faucibus quam quis, sagittis ipsum. Integer condimentum, felis id ultricies blandit, velit eros imperdiet elit, ut vulputate arcu purus sed elit. Morbi efficitur cursus dictum. Cras sagittis sapien vel quam euismod feugiat. Cras a risus quis libero venenatis aliquam. In tristique maximus convallis",
-      date: moment(1439201833740).calendar(),
-      imageURL: "http://www.liftopia.com/media/product/5051/102416_Aspen-Snowmass-Perfect-Summer-Package.jpg",
-      countOrder: 0,
-      comments: [
-        {name: "Bob",
-          comment: "That was crazy cool"
-        }
-      ],
-    },
-    {title: "Boulder, CO",
-      vote: 12,
-      author: "Jon Snow",
-      content: "Vestibulum id urna nec massa congue gravida a ut nisi. Nullam sit amet urna rutrum, faucibus quam quis, sagittis ipsum. Integer condimentum, felis id ultricies blandit, velit eros imperdiet elit, ut vulputate arcu purus sed elit. Morbi efficitur cursus dictum. Cras sagittis sapien vel quam euismod feugiat. Cras a risus quis libero venenatis aliquam. In tristique maximus convallis.",
-      date: moment(1439313833740).calendar(),
-      imageURL: "https://dombestlist.files.wordpress.com/2011/08/boulder-colorado.jpg",
-      countOrder: 1,
-      comments: [
-        {name: "Tess",
-          comment: "what's going on?"
-        },
-        {name: "Brian",
-          comment: "Nice picture!"
-        },
-        {name: "Joe",
-          comment: "I wish I lived there"
-        }
-      ],
-    },
-    {title: "Denver, CO",
-      vote: 9,
-      author: "Daenerys Targaryen",
-      content: "Vestibulum id urna nec massa congue gravida a ut nisi. Nullam sit amet urna rutrum, faucibus quam quis, sagittis ipsum. Integer condimentum, felis id ultricies blandit, velit eros imperdiet elit, ut vulputate arcu purus sed elit. Morbi efficitur cursus dictum. Cras sagittis sapien vel quam euismod feugiat. Cras a risus quis libero venenatis aliquam. In tristique maximus convallis.",
-      imageURL: "http://www.weddingstylemagazine.com/sites/default/files/denver-colorado-travel-2014-004.jpg",
-      countOrder: 2,
-      date: moment(1439413433740).calendar(),
-      comments: [
-        {name: "Tracey",
-          comment: "That's one big city!"
-        },
-        {name: "Sam",
-          comment: "My name isn't who I say I am, I say I am who you say you think I am"
-        }
-      ],
-    },
-    {title: "Colorado Springs, CO",
-      vote: 4,
-      author: "Tyrion Lannister",
-      content: "Vestibulum id urna nec massa congue gravida a ut nisi. Nullam sit amet urna rutrum, faucibus quam quis, sagittis ipsum. Integer condimentum, felis id ultricies blandit, velit eros imperdiet elit, ut vulputate arcu purus sed elit. Morbi efficitur cursus dictum. Cras sagittis sapien vel quam euismod feugiat. Cras a risus quis libero venenatis aliquam. In tristique maximus convallis.",
-      date: moment(1439496091029).calendar(),
-      imageURL: "http://pics4.city-data.com/cpicv/vfiles9625.jpg",
-      countOrder: 3,
-      comments: [
-      ],
-    },
-    {title: "Pueblo, CO",
-      vote: -1,
-      author: "Jaime Lannister",
-      content: "Vestibulum id urna nec massa congue gravida a ut nisi. Nullam sit amet urna rutrum, faucibus quam quis, sagittis ipsum. Integer condimentum, felis id ultricies blandit, velit eros imperdiet elit, ut vulputate arcu purus sed elit. Morbi efficitur cursus dictum. Cras sagittis sapien vel quam euismod feugiat. Cras a risus quis libero venenatis aliquam. In tristique maximus convallis.",
-      date: moment(1439566425479).calendar(),
-      imageURL: "http://www.liveinpueblo.com/briefcase/99572_9232014102144AM34267.JPG",
-      countOrder: 4,
-      comments: [
-        {name: "Josh",
-          comment: "food for thought"
-        },
-        {name: "Rumge",
-          comment: "Thats intersting! #GotMilk"
-        }
-      ],
-    },
-    {title: "Durango, CO",
-      vote: -5,
-      author: "Petyr Baelish",
-      content: "Vestibulum id urna nec massa congue gravida a ut nisi. Nullam sit amet urna rutrum, faucibus quam quis, sagittis ipsum. Integer condimentum, felis id ultricies blandit, velit eros imperdiet elit, ut vulputate arcu purus sed elit. Morbi efficitur cursus dictum. Cras sagittis sapien vel quam euismod feugiat. Cras a risus quis libero venenatis aliquam. In tristique maximus convallis.",
-      date: moment(1439569455453).calendar(),
-      imageURL: "http://www.gatewayreservations.com/wp-content/uploads/2014/10/durango-colorado-460x247.jpg",
-      countOrder: 5,
-      comments: [
-        {name: "Bobby",
-          comment: "Hey, man nice story about that city"
-        }
-      ],
-    }
-  ];
+myApp.controller("users", ["$scope","$firebaseArray", function($scope, $firebaseArray) {
+  var postList = new Firebase("https://dazzling-inferno-3947.firebaseio.com/reddit")
+  $scope.posts = $firebaseArray(postList);
+
+  //$scope.posts = [
+    //{title: "Aspen, CO",
+      //vote: -9,
+      //author: "Ned Stark",
+      //content: "Vestibulum id urna nec massa congue gravida a ut nisi. Nullam sit amet urna rutrum, faucibus quam quis, sagittis ipsum. Integer condimentum, felis id ultricies blandit, velit eros imperdiet elit, ut vulputate arcu purus sed elit. Morbi efficitur cursus dictum. Cras sagittis sapien vel quam euismod feugiat. Cras a risus quis libero venenatis aliquam. In tristique maximus convallis.Vestibulum id urna nec massa congue gravida a ut nisi. Nullam sit amet urna rutrum, faucibus quam quis, sagittis ipsum. Integer condimentum, felis id ultricies blandit, velit eros imperdiet elit, ut vulputate arcu purus sed elit. Morbi efficitur cursus dictum. Cras sagittis sapien vel quam euismod feugiat. Cras a risus quis libero venenatis aliquam. In tristique maximus convallis",
+      //date: moment(1439201833740).calendar(),
+      //imageURL: "http://www.liftopia.com/media/product/5051/102416_Aspen-Snowmass-Perfect-Summer-Package.jpg",
+      //countOrder: 0,
+      //comments: [
+        //{name: "Bob",
+          //comment: "That was crazy cool"
+        //}
+      //],
+    //},
+    //{title: "Boulder, CO",
+      //vote: 12,
+      //author: "Jon Snow",
+      //content: "Vestibulum id urna nec massa congue gravida a ut nisi. Nullam sit amet urna rutrum, faucibus quam quis, sagittis ipsum. Integer condimentum, felis id ultricies blandit, velit eros imperdiet elit, ut vulputate arcu purus sed elit. Morbi efficitur cursus dictum. Cras sagittis sapien vel quam euismod feugiat. Cras a risus quis libero venenatis aliquam. In tristique maximus convallis.",
+      //date: moment(1439313833740).calendar(),
+      //imageURL: "https://dombestlist.files.wordpress.com/2011/08/boulder-colorado.jpg",
+      //countOrder: 1,
+      //comments: [
+        //{name: "Tess",
+          //comment: "what's going on?"
+        //},
+        //{name: "Brian",
+          //comment: "Nice picture!"
+        //},
+        //{name: "Joe",
+          //comment: "I wish I lived there"
+        //}
+      //],
+    //},
+    //{title: "Denver, CO",
+      //vote: 9,
+      //author: "Daenerys Targaryen",
+      //content: "Vestibulum id urna nec massa congue gravida a ut nisi. Nullam sit amet urna rutrum, faucibus quam quis, sagittis ipsum. Integer condimentum, felis id ultricies blandit, velit eros imperdiet elit, ut vulputate arcu purus sed elit. Morbi efficitur cursus dictum. Cras sagittis sapien vel quam euismod feugiat. Cras a risus quis libero venenatis aliquam. In tristique maximus convallis.",
+      //imageURL: "http://www.weddingstylemagazine.com/sites/default/files/denver-colorado-travel-2014-004.jpg",
+      //countOrder: 2,
+      //date: moment(1439413433740).calendar(),
+      //comments: [
+        //{name: "Tracey",
+          //comment: "That's one big city!"
+        //},
+        //{name: "Sam",
+          //comment: "My name isn't who I say I am, I say I am who you say you think I am"
+        //}
+      //],
+    //},
+    //{title: "Colorado Springs, CO",
+      //vote: 4,
+      //author: "Tyrion Lannister",
+      //content: "Vestibulum id urna nec massa congue gravida a ut nisi. Nullam sit amet urna rutrum, faucibus quam quis, sagittis ipsum. Integer condimentum, felis id ultricies blandit, velit eros imperdiet elit, ut vulputate arcu purus sed elit. Morbi efficitur cursus dictum. Cras sagittis sapien vel quam euismod feugiat. Cras a risus quis libero venenatis aliquam. In tristique maximus convallis.",
+      //date: moment(1439496091029).calendar(),
+      //imageURL: "http://pics4.city-data.com/cpicv/vfiles9625.jpg",
+      //countOrder: 3,
+      //comments: [
+      //],
+    //},
+    //{title: "Pueblo, CO",
+      //vote: -1,
+      //author: "Jaime Lannister",
+      //content: "Vestibulum id urna nec massa congue gravida a ut nisi. Nullam sit amet urna rutrum, faucibus quam quis, sagittis ipsum. Integer condimentum, felis id ultricies blandit, velit eros imperdiet elit, ut vulputate arcu purus sed elit. Morbi efficitur cursus dictum. Cras sagittis sapien vel quam euismod feugiat. Cras a risus quis libero venenatis aliquam. In tristique maximus convallis.",
+      //date: moment(1439566425479).calendar(),
+      //imageURL: "http://www.liveinpueblo.com/briefcase/99572_9232014102144AM34267.JPG",
+      //countOrder: 4,
+      //comments: [
+        //{name: "Josh",
+          //comment: "food for thought"
+        //},
+        //{name: "Rumge",
+          //comment: "Thats intersting! #GotMilk"
+        //}
+      //],
+    //},
+    //{title: "Durango, CO",
+      //vote: -5,
+      //author: "Petyr Baelish",
+      //content: "Vestibulum id urna nec massa congue gravida a ut nisi. Nullam sit amet urna rutrum, faucibus quam quis, sagittis ipsum. Integer condimentum, felis id ultricies blandit, velit eros imperdiet elit, ut vulputate arcu purus sed elit. Morbi efficitur cursus dictum. Cras sagittis sapien vel quam euismod feugiat. Cras a risus quis libero venenatis aliquam. In tristique maximus convallis.",
+      //date: moment(1439569455453).calendar(),
+      //imageURL: "http://www.gatewayreservations.com/wp-content/uploads/2014/10/durango-colorado-460x247.jpg",
+      //countOrder: 5,
+      //comments: [
+        //{name: "Bobby",
+          //comment: "Hey, man nice story about that city"
+        //}
+      //],
+    //}
+  //];
   $scope.lists = ["foo", "bar"];
   $scope.newComment = function(name, comment, counter) {
     var object = {};
+    var array = [];
     object.name = name;
     object.comment = comment;
+    array.push(object);
     var numb = parseInt(this.countOrder);
-    $scope.posts[numb].comments.push(object);
+    if ($scope.posts[numb].comments === undefined) {
+      $scope.posts[numb].comments = array;
+    } else {
+      $scope.posts[numb].comments.push(object);
+    }
+    console.log($scope.posts[numb]);
+
+    //$scope.posts.$save(numb);
   };
   $scope.newPost = function() {
     var object = {};
@@ -104,16 +116,18 @@ myApp.controller("users", ["$scope", function($scope) {
     object.title = this.title;
     object.author = this.author;
     object.content = this.content;
-    object.date = Date.now();
+    object.date = moment(Date.now()).calendar().toString();
     object.imageURL = this.imageURL;
     object.countOrder = $scope.posts.length;
-    object.comments = [];
-    $scope.posts.push(object);
+    $scope.posts.$add(object);
+    console.log(object);
   };
   $scope.upvote = function(count) {
     $scope.posts[count].vote++;
+    $scope.posts.$save(count);
   };
   $scope.downvote = function(count) {
     $scope.posts[count].vote--;
+    $scope.posts.$save(count);
   };
 }]);
